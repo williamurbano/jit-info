d68a15fd5618353ae79e91f7e6393c8fa3b6824f
William Urbano <contato@williamurbano.com.br>
2014-05-15 17:21:22 -0300

    Configuração do Grunt.js e Página Inicial
    Alterada configuração do Grunt.js e layout da página inicial

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,8 @@ module.exports = function(grunt) {
       'build/css',
       'build/js',
       'build/images',
-      'build/vendor'
+      'build/vendor',
+      '*.html'
     ],
 
     less: {
@@ -38,16 +39,34 @@ module.exports = function(grunt) {
     },
 
     concat: {
-      options: {
-        banner: '<%= banner %>',
-        separator: '\n\n'
-      },
       development: {
-        src: [
-        'src/js/<%= pkg.name %>.js'
-        ],
+        options: {
+          banner: '<%= banner %>',
+          separator: '\n\n'
+        },
+        src: ['src/js/<%= pkg.name %>.js'],
         dest: 'build/js/<%= pkg.name %>.js'
-      }
+      }/*,
+      clientes: {
+        src: ['src/partials/head.html', 'src/pages/clientes.html', 'src/partials/footer.html'],
+        dest: 'clientes.html'
+      },
+      contato: {
+        src: ['src/partials/head.html', 'src/pages/contato.html', 'src/partials/footer.html'],
+        dest: 'contato.html'
+      },
+      index: {
+        src: ['src/partials/head.html', 'src/pages/index.html', 'src/partials/footer.html'],
+        dest: 'index.html'
+      },
+      servicos: {
+        src: ['src/partials/head.html', 'src/pages/servicos.html', 'src/partials/footer.html'],
+        dest: 'servicos.html'
+      },
+      sobre: {
+        src: ['src/partials/head.html', 'src/pages/sobre.html', 'src/partials/footer.html'],
+        dest: 'sobre.html'
+      }*/
     },
 
     uglify: {
@@ -116,14 +135,15 @@ module.exports = function(grunt) {
     }
   });
 
-grunt.loadNpmTasks('grunt-contrib-less');
-grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-concat');
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-replace');
 
-grunt.registerTask('default', ['watch']);
-grunt.registerTask('build', ['clean', 'less', 'concat', 'uglify', 'copy']);
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['clean', 'less', 'concat', 'uglify', 'copy']);
 
 };
