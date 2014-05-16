6f497005ff49c543e45c76cdadd537d59fc2271a
William Urbano <contato@williamurbano.com.br>
2014-05-16 10:42:38 -0300

    .gitignore
    Removido diretório build do .gitignore

---
+++ build/js/jit-info.js
@@ -0,0 +1,15 @@
+/*! jit-info 2014-05-16 */
+var menu    = document.getElementById("menu"),
+    anchors = menu.getElementsByTagName("a"),
+    actived = false;
+
+for(var i in anchors) {
+  if(anchors[i].href == window.location.href) {
+    anchors[i].className += "active";
+    actived = true;
+  }
+}
+
+if(!actived) {
+  anchors[0].className += "active";
+}
