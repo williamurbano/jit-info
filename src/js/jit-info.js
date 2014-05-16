201a8143190320f0671a10df5419d7bc6fd44dd6
William Urbano <contato@williamurbano.com.br>
2014-05-16 10:37:20 -0300

    Menu ativo
    Criada função javascript para destacar o menu ativo

---
+++ src/js/jit-info.js
@@ -0,0 +1,14 @@
+var menu    = document.getElementById("menu"),
+    anchors = menu.getElementsByTagName("a"),
+    actived = false;
+
+for(var i in anchors) {
+  if(anchors[i].href == window.location.href) {
+    anchors[i].className += "active";
+    actived = true;
+  }
+}
+
+if(!actived) {
+  anchors[0].className += "active";
+}
